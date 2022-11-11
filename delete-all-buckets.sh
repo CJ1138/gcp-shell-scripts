b497979ed03bb7d847493997c14d01545a35516e
vagrant <vagrant@vagrant.vm>
2022-11-11 11:31:30 +0000

    Created initial repo

---
+++ delete-all-buckets.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+#      _          _      _       _                            _            _     
+#  ___| |__  _ __(_)___ (_) ___ | |__  _ __  ___  ___  _ __  | |_ ___  ___| |__  
+# / __| '_ \| '__| / __|| |/ _ \| '_ \| '_ \/ __|/ _ \| '_ \ | __/ _ \/ __| '_ \ 
+#| (__| | | | |  | \__ \| | (_) | | | | | | \__ \ (_) | | | || ||  __/ (__| | | |
+# \___|_| |_|_|  |_|___// |\___/|_| |_|_| |_|___/\___/|_| |_(_)__\___|\___|_| |_|
+#                     |__/                                                      
+#
+# Deletes all GSC buckets in a GCP project
+
+ProjectId=$(gcloud config get-value project)
+
+echo "You are about to delete all buckets in project ID: $ProjectId. Type YES to proceed:"
+read confirmation
+
+if [ $confirmation == "YES" ]
+then
+  count=0
+  for x in `gsutil ls`
+    do gsutil rm -r $x
+    ((count=$count+1))
+  done
+  echo "$count buckets sucessfully deleted."
+else
+  echo "Aborted"
+fi
---
+++ make-public-bucket.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+#      _          _      _       _                            _            _     
+#  ___| |__  _ __(_)___ (_) ___ | |__  _ __  ___  ___  _ __  | |_ ___  ___| |__  
+# / __| '_ \| '__| / __|| |/ _ \| '_ \| '_ \/ __|/ _ \| '_ \ | __/ _ \/ __| '_ \ 
+#| (__| | | | |  | \__ \| | (_) | | | | | | \__ \ (_) | | | || ||  __/ (__| | | |
+# \___|_| |_|_|  |_|___// |\___/|_| |_|_| |_|___/\___/|_| |_(_)__\___|\___|_| |_|
+#                     |__/                                                      
+#
+# Creates a public Google Cloud Storage bucket 
+
+
+gsutil mb gs://$1
+
+
+gsutil iam ch allUsers:objectViewer gs://$1
+echo "Bucket created sucessfully!"
+echo "Public URL: https://storage.googleapis.com/$1"
